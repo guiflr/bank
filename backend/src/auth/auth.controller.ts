3535bc9d6a8d51b65fbfce1bbde1ada3dd8556f3
Guilherme Felipe <guiflr97@gmail.com>
2026-03-14 11:55:15 -0500

    feat: add auth controller implementation

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthToken } from './auth.token';
+import { AuthService } from './auth.service';
+import type { SignInDto } from './dtos/signIn';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly authToken: AuthToken,
+  ) {}
+
+  @Post('signin')
+  async signIn(@Body() body: SignInDto): Promise<{ token: string }> {
+    const user = await this.authService.signIn(body);
+    const token = await this.authToken.generateToken(user);
+    return { token };
+  }
+}
