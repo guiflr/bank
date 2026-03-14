d3374d5e6cb592035c47ed84833509e755e2449a
Guilherme Felipe <guiflr97@gmail.com>
2026-03-14 12:04:12 -0500

    refactor: remove token generator from auth service implementation

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,14 +3,12 @@ import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import type { AuthHash } from './domain/hash';
 import type { AuthRepository } from './domain/repository';
 import { SignInDto, SignInResponse } from './dtos/signIn';
-import type { Token } from './domain/token';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject('AuthRepository') private readonly authRepository: AuthRepository,
     @Inject('AuthHash') private readonly authHash: AuthHash,
-    @Inject('Token') private readonly token: Token,
   ) {}
 
   async signIn({ username, password }: SignInDto): Promise<SignInResponse> {
@@ -30,9 +28,5 @@ export class AuthService {
     }
 
     return { id: user.id, username: user.username };
-  }
-
-  async generateToken(data: SignInResponse): Promise<string> {
-    return this.token.generateToken(data);
   }
 }
