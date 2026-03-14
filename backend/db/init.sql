399cde99464dbac693fc3324d5a58d9c6379b2fb
Guilherme Felipe <guiflr97@gmail.com>
2026-03-14 18:01:55 -0500

    feat:  add table user script

---
+++ backend/db/init.sql
@@ -0,0 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
+);
