21430d1213300889ee955aae3243228c9c8b646e
Guilherme Felipe <guiflr97@gmail.com>
2026-03-15 13:02:35 -0500

    feat: config transactions module providers

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+
+import { AUTH_TOKEN } from '../auth/domain/token';
+import { AuthJwt } from '../auth/libs/jwt/auth.jwt';
+import { KnexService } from '../infra/knex/knex.service';
 import { TransactionsService } from './transactions.service';
+import { TRANSACTION_REPOSITORY } from './domain/repository';
+import { TransactionsController } from './transactions.controller';
+import { TransactionKnexRepository } from './infra/transaction.knex.repository';
 
 @Module({
-  providers: [TransactionsService]
+  providers: [
+    TransactionsService,
+    KnexService,
+    AuthJwt,
+    TransactionKnexRepository,
+    {
+      provide: AUTH_TOKEN,
+      useExisting: AuthJwt,
+    },
+    {
+      provide: TRANSACTION_REPOSITORY,
+      useExisting: TransactionKnexRepository,
+    },
+  ],
+  controllers: [TransactionsController],
 })
 export class TransactionsModule {}
