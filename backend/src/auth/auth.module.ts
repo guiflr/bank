53219d3349dd57892bcb0fc29e18b0349b0de70e
Guilherme Felipe <guiflr97@gmail.com>
2026-03-15 09:07:31 -0500

    refactor: update auth module to use implementations

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 
+import { AUTH_TOKEN } from './domain/token';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { KnexModule } from '../infra/knex/knex.module';
+import { AuthKnexRepository } from './infra/knex/auth.knex.repository';
+import { Bcrypt } from './libs/bcrypt/auth.bcrypt';
+import { AuthJwt } from './libs/jwt/auth.jwt';
+import { AUTH_HASH } from './domain/hash';
+import { AUTH_REPOSITORY } from './domain/repository';
 
 @Module({
+  imports: [KnexModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    AuthJwt,
+    Bcrypt,
+    AuthKnexRepository,
+    {
+      provide: AUTH_TOKEN,
+      useExisting: AuthJwt,
+    },
+    {
+      provide: AUTH_HASH,
+      useExisting: Bcrypt,
+    },
+    {
+      provide: AUTH_REPOSITORY,
+      useExisting: AuthKnexRepository,
+    },
+  ],
 })
 export class AuthModule {}
