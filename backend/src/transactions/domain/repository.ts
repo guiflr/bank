8398ad7d2642d48e2df4a446d99714b9f7e459a1
Guilherme Felipe <guiflr97@gmail.com>
2026-03-15 16:27:52 -0500

    feat: add to transaction repository new implementations

--- backend/src/transactions/domain/repository.ts
+++ backend/src/transactions/domain/repository.ts
@@ -2,8 +2,25 @@ export type Balance = {
   balance: number;
 };
 
+export type Account = {
+  account: string;
+};
+
+export type DepositDAO = {
+  account: string;
+  balance: number;
+  type: 'deposit';
+};
+
+export type DepositResponseDAO = {
+  account: string;
+  balance: number;
+};
+
 export const TRANSACTION_REPOSITORY = 'TRANSACTION_REPOSITORY';
 
 export interface TransactionRepository {
   getBalance(account: string): Promise<Balance | null>;
+  getAccount(account: string): Promise<Account | null>;
+  deposit(data: DepositDAO): Promise<void>;
 }
--- backend/src/transactions/infra/transaction.knex.repository.ts
+++ backend/src/transactions/infra/transaction.knex.repository.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Balance, TransactionRepository } from '../domain/repository';
+
+import {
+  Account,
+  Balance,
+  DepositDAO,
+  TransactionRepository,
+} from '../domain/repository';
 import { KnexService } from '../../infra/knex/knex.service';
 
 @Injectable()
@@ -15,5 +21,21 @@ export class TransactionKnexRepository implements TransactionRepository {
     if (!data || data.balance === null) return null;
 
     return { balance: data.balance };
+  }
+
+  async getAccount(account: string): Promise<Account | null> {
+    const data = await this.knexService
+      .db('transactions')
+      .select('account')
+      .where({ account })
+      .first();
+
+    if (!data?.account) return null;
+
+    return data;
+  }
+
+  async deposit(data: DepositDAO): Promise<void> {
+    await this.knexService.db('transactions').insert(data);
   }
 }
