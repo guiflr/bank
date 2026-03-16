bc213a0464681db2d11c11084858787b19696ea7
Guilherme Felipe <guiflr97@gmail.com>
2026-03-15 21:18:40 -0500

    feat: config tailwind

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,15 @@
+import './globals.css';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="pt-BR">
+      <body className="min-h-screen bg-white text-black antialiased dark:bg-black dark:text-white">
+        {children}
+      </body>
     </html>
   );
 }
