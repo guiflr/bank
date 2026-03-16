5770c9ce08eadd085fa5bc5fe7e6205f3a570035
Guilherme Felipe <guiflr97@gmail.com>
2026-03-15 22:53:41 -0500

    feat: call login action on login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,20 +1,33 @@
 import Input from "../components/Input";
+import { signIn } from "./actions";
 
-export default function Login() {
+type LoginPageProps = {
+  searchParams?: Promise<{
+    error?: string;
+  }>;
+};
+
+export default async function Login({ searchParams }: LoginPageProps) {
+  const resolvedParams = searchParams ? await searchParams : undefined;
+  const error = resolvedParams?.error;
+
   return (
-    <main className="grid min-h-screen w-full place-items-center px-6 py-12">
-      <section className="w-full max-w-md rounded-2xl border-2 border-black bg-white p-8 dark:border-white dark:bg-black">
-        <form className="grid gap-5">
-          <Input name="username" type="text" label="Username" />
-          <Input name="password" type="password" label="Password" />
-          <button
-            type="submit"
-            className="mt-2 border border-black bg-black px-4 py-2 text-xs rounded-md uppercase tracking-[0.3em] text-white transition hover:cursor-pointer dark:border-white dark:bg-white dark:text-black"
-          >
-            Prosseguir
-          </button>
-        </form>
-      </section>
-    </main>
+    <>
+      <main className="grid min-h-screen w-full place-items-center px-6 py-12">
+        <section className="w-full max-w-md rounded-2xl border-2 border-black bg-white p-8 dark:border-white dark:bg-black">
+          {error && <p className="text-red-600 text-center">{error}</p>}
+          <form className="grid gap-5" action={signIn}>
+            <Input name="username" type="text" label="Username" />
+            <Input name="password" type="password" label="Password" />
+            <button
+              type="submit"
+              className="mt-2 border border-black bg-black px-4 py-2 text-xs rounded-md uppercase tracking-[0.3em] text-white transition hover:cursor-pointer dark:border-white dark:bg-white dark:text-black"
+            >
+              Prosseguir
+            </button>
+          </form>
+        </section>
+      </main>
+    </>
   );
 }
